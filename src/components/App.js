163b4ad486b39635ef941f8bf0c9cbaa6fea3abe
WalukaUI <cwaluka@yahoo.com>
2021-08-03 22:50:30 -0700

    create basic folders

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+
+// import './App.css';
+import Players from "./layers/players";
+
+function App() {
+  return (
+    <div className="App">
+    <Players />
+    </div>
+  );
+}
+
+export default App;
