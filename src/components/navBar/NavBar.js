20f1d0637d8e5424d98a2ff4eed46c539f34f0c8
WalukaUI <cwaluka@yahoo.com>
2021-08-08 00:17:02 -0700

    edit NavBarlinks

--- src/components/navBar/NavBar.js
+++ src/components/navBar/NavBar.js
@@ -1,35 +1,61 @@
-import React from "react"
+import React from "react";
 import { Link } from "react-router-dom";
 
-function NavBar(){
-    return<nav className="navbar navbar-expand-lg navbar-light bg-light">
-  <a className="navbar-brand" href="#">CrickUSA</a>
-  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span className="navbar-toggler-icon"></span>
-  </button>
+function NavBar() {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <a className="navbar-brand" href="#">
+        CrickUSA
+      </a>
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon"></span>
+      </button>
 
-  <div className="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul className="navbar-nav mr-auto">
-      <li className="nav-item active">
-      <Link to={`/`}>
-        <a className="nav-link" href="#">Home</a>
-      </Link>
-      </li>
-      <li className="nav-item">
-        <a className="nav-link" href="#">Teams</a>
-      </li>
-      <li className="nav-item">
-        <a className="nav-link" href="#">Venues</a>
-      </li>
-    </ul>
+      <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul className="navbar-nav mr-auto">
+          <li className="nav-item active">
+            <Link to={`/`}>
+              <a className="nav-link" href="#">
+                Home
+              </a>
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to={`/teams`}>
+              <a className="nav-link" href="#">
+                Teams
+              </a>
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to={`/venues`}>
+              <a className="nav-link" href="#">
+                Venues
+              </a>
+            </Link>
+          </li>
+        </ul>
 
-    <form className="form-inline my-2 my-lg-0">
-      <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
-      {/* <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button> */}
-    </form>
-
-  </div>
-</nav>
+        <form className="form-inline my-2 my-lg-0">
+          <input
+            className="form-control mr-sm-2"
+            type="search"
+            placeholder="Search"
+            aria-label="Search"
+          />
+          {/* <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button> */}
+        </form>
+      </div>
+    </nav>
+  );
 }
 
-export default NavBar
+export default NavBar;
