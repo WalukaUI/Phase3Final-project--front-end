9181a46c5f470d77b0902cc62ad4d5399505c1df
WalukaUI <cwaluka@yahoo.com>
2021-08-08 00:06:37 -0700

    edit player file

--- src/components/playerProfile/Player.js
+++ src/components/playerProfile/Player.js
@@ -4,16 +4,22 @@ import { useParams } from "react-router-dom"
 const URL = "http://127.0.0.1:9393/";
 
 function PlayerProfile(){
-    const [singlePlayer, setsinglePlayer] = useState(null);
+    const [singlePlayer, setsinglePlayer] = useState([]);
     const params = useParams()
     useEffect(() => {
-        fetch(`${URL}/players/${params.id}`)
-          .then(r => r.json())
-          .then(data => setsinglePlayer(data))
-      }, [params.id])
+      fetch(`${URL}/players/${params.id}`)
+        .then((r) => r.json())
+        .then((data) => setsinglePlayer(data));
+    }, [params.id]);
 
 
-    return <p>this is a profile</p>
+    return <div>
+       <p>{singlePlayer.name}</p>
+       <p>{singlePlayer.age}</p>
+       <p>{singlePlayer.skill}</p>
+       <p>{singlePlayer.team}</p>
+    </div>
+    
 }
 
 export default PlayerProfile
