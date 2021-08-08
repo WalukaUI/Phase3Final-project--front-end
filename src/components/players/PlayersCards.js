9a735350c3ba0d81a8d8844f10ccb1ee784cb4e9
WalukaUI <cwaluka@yahoo.com>
2021-08-08 15:28:56 -0700

    delete a player

--- src/components/players/PlayersCards.js
+++ src/components/players/PlayersCards.js
@@ -2,7 +2,12 @@ import React from 'react';
 import { Link } from "react-router-dom";
 import "../players/players.css"
 
-function PlayersCards({name, age, skill, id}){
+function PlayersCards({name, age, skill, id, deletePlayer}){
+
+  function onclickOfDelete(e){
+    e.preventDefault()
+    deletePlayer(id)
+  }
 
     return <div className="cardstyle">
     <img src="..." className="card-img-top" alt="..."/>
@@ -11,8 +16,10 @@ function PlayersCards({name, age, skill, id}){
     <p className="card-text">{skill}</p>
     <p className="card-text">Age: {age}</p>
     <Link to={`/players/${id}`}>
-    <a href="!#" className="btn btn-primary">More details</a>
+       <a href="!#" className="btn btn-primary">More details</a>
     </Link>
+    <a href="!#" className="btn btn-primary">Edit</a>
+    <a  href="!#" onClick={onclickOfDelete} className="btn btn-primary">Delete</a>
   </div>
 </div>
 }
