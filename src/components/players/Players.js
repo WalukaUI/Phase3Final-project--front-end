85e38ac42f494db00439d5cb7c007376db70880b
WalukaUI <cwaluka@yahoo.com>
2021-08-09 00:50:37 -0700

    add player form post

--- src/components/players/Players.js
+++ src/components/players/Players.js
@@ -8,7 +8,9 @@ const URL = "http://127.0.0.1:9393";
 function Players() {
   const [allPlayers, setAllPlayers] = useState([]);
   const [addplayerForm, setAddPlayerForm] = useState(false);
-  const [addPlayer, setAddPlayer] = useState(null);
+  const [addPlayer, setAddPlayer] = useState({});
+
+//GET PLAYERS
 
   useEffect(() => {
     let getrequestOptions = {
@@ -26,6 +28,8 @@ function Players() {
     setAllPlayers(newPlayers);
   }
 
+//UPDATE PLAYER
+
   function updatePlayer(playerObject) {
     fetch(`${URL}/players/${playerObject.id}`, {
       method: "PATCH",
@@ -42,13 +46,39 @@ function Players() {
         );
         setAllPlayers([...newPlayers, player]);
       });
-    //
-    //
   }
+
+//CREATE PLAYER
+
+function createPlayer(newPlayerObject) {
+  fetch(`${URL}/players`, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(newPlayerObject),
+  })
+    .then((res) => res.json())
+    .then((player) => console.log(player))
+      // setAllPlayers([...newPlayer, player]);
+}
+
+//----------------------------------------------------------------------
 
   function toggleForm(e) {
     e.preventDefault();
     setAddPlayerForm(!addplayerForm);
+  }
+  function createPlayer(e){
+    e.preventDefault();
+    createPlayer(addPlayer);
+  }
+  function handleAddPlayer(e){
+    e.preventDefault();
+    let newPlayer = { ...addPlayer, [e.target.name]: e.target.value }
+    setAddPlayer(newPlayer)
+
   }
 
   const playerstoCard = allPlayers.map((card) => {
@@ -72,34 +102,38 @@ function Players() {
         </button>
       </div>
       <div className={addplayerForm ? "display" : "hidden"}>
-        <form>
+        <form onSubmit={createPlayer}>
           <label>
             Name
             <input
               className="form-control form-control-sm"
               type="text"
+              name="name"
               placeholder="Name"
+              onChange={handleAddPlayer}
             />
           </label>
           <label>
             Age
             <input
               className="form-control form-control-sm"
-              type="text"
+              type="number"
+              name="age"
               placeholder="Age"
+              onChange={handleAddPlayer}
             />
           </label>
           <label>
             Skill
-            <input
-              className="form-control form-control-sm"
-              type="text"
-              placeholder="Skill"
-            />{" "}
+            <select className="form-select" name="skill" onChange={handleAddPlayer}>
+              <option value="Allrounder">Allrounder</option>
+              <option value="Batsman">Batsman</option>
+              <option value="Bowler">Bowler</option>
+            </select>
           </label>
           <label>
             Team
-            <select className="form-select" name="Team">
+            <select className="form-select" name="team" onChange={handleAddPlayer}>
               <option value="1">SL</option>
               <option value="2">IND</option>
               <option value="3">AUS</option>
@@ -112,6 +146,7 @@ function Players() {
               className="form-select"
               name="playing_category"
               aria-label="Default select example"
+              onChange={handleAddPlayer}
             >
               <option value="1">T20</option>
               <option value="2">ODI</option>
