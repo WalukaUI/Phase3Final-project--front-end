d00dec3ddcf31e62018e15bc7918d04d84c0941f
WalukaUI <cwaluka@yahoo.com>
2021-08-15 12:42:45 -0700

    card animated

--- src/components/players/PlayersCards.js
+++ src/components/players/PlayersCards.js
@@ -2,8 +2,8 @@ import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import "../players/players.css";
 import EditPlayer from "./EditPlayer";
-import Aos from "aos"
-import "aos/dist/aos.css"
+import Aos from "aos";
+import "aos/dist/aos.css";
 
 function PlayersCards({ card, deletePlayer, updatePlayer, idx }) {
   const [isOpen, setIsOpen] = useState(false);
@@ -19,34 +19,38 @@ function PlayersCards({ card, deletePlayer, updatePlayer, idx }) {
     setUpdateData(card);
   }
 
-  useEffect(()=>{
+  useEffect(() => {
     Aos.init({});
-  },[])
+  }, []);
   return (
     <>
-    {/* data-aos={idx>9?"fade-up":null} */}
-      <div  className="cardstyle">
-        <div>
-        <img src={card.image_url?card.image_url:null} className="card-img-top" alt="..." />
-        <h4 className="card-title">{card.name}</h4>
-        </div>
-        <div>
-
-        </div>
-       
-        <div className="card-body">
-        <p className="card-text">{card.skill}</p>
-          <p className="card-text">Age: {card.age}</p>
-          <div className="btnbox">
-            <Link to={`/players/${card.id}`}>
-              <button className="btn btn-primary">More details</button>
-            </Link>
-            <button onClick={onClickofEdit} className="btn btn-info">
-              Edit
-            </button>
-            <button onClick={onClickOfDelete} className="btn btn-warning">
-              Delete
-            </button>
+      {/* data-aos={idx>9?"fade-up":null} */}
+      <div className="cardstyle">
+        <div className="flip-card-inner">
+          <div className="flip-card-front">
+            <img
+              src={card.image_url ? card.image_url : null}
+              className="card-img-top"
+              alt="..."
+            />
+            <h5 className="card-title">{card.name}</h5>
+          </div>
+          <div className="flip-card-back">
+            <div className="card-body">
+              <p className="card-text">{card.skill}</p>
+              <p className="card-text">Age: {card.age}</p>
+              <div className="btnbox">
+                <Link to={`/players/${card.id}`}>
+                  <button className="btn btn-primary">More details</button>
+                </Link>
+                <button onClick={onClickofEdit} className="btn btn-info">
+                  Edit
+                </button>
+                <button onClick={onClickOfDelete} className="btn btn-warning">
+                  Delete
+                </button>
+              </div>
+            </div>
           </div>
         </div>
       </div>
--- src/components/players/playerProfile/Player.js
+++ src/components/players/playerProfile/Player.js
@@ -55,8 +55,6 @@ function PlayerProfile() {
         </div>
       </div>
 
-
-
     </>
 }
 
